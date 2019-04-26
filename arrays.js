129f5ec2cf56415a9bcc1c9c316d31fd806f7c19
Jonathan Fink <collegepulsemag@gmail.com>
2019-04-26 05:54:05 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(Array, x){
+  var myArray = Array;
+  var myOtherArray = myArray[x, ...Array];
+}
+
+function destructivelyAddElementToBeginningOfArray(Array, x) {
+  var myArray = Array;
+  myArray.unshift(x)
+  return myArray
+}
