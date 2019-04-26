48e3695253ed592a9a9a71efa8ee589d2e7eb55c
Jonathan Fink <collegepulsemag@gmail.com>
2019-04-26 06:04:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(ArrayP, x){
-  var myArray = ArrayP;
+function addElementToBeginningOfArray(Array, element){
   var myOtherArray = myArray[x, ...Array];
 }
 
